9a6f4971b8d973c8470f5dfe6db6c4701a51cfb8
Chris Bridgewater <cgbridgewater@outlook.com>
2023-07-09 02:44:25 -0700

    first commit

---
+++ client/src/components/TitlePage.jsx
@@ -0,0 +1,39 @@
+
+
+const Title = () => {
+    
+
+    return(    
+
+        <div>
+            <h1 className="banner">Can You <em>Remember?</em></h1>
+            <h1 className="banner-sm">Draw the logo from memory!!</h1>
+            <main>
+                <section>
+                    <p>C</p>
+                    <p>A</p>
+                    <p>N</p>
+                    <p>&nbsp;</p>
+                    <p>&nbsp;</p>
+                    <p>Y</p>
+                    <p>O</p>
+                    <p>U</p>
+                    <p>&nbsp;</p>
+                    <p>&nbsp;</p>
+                    <p>R</p>
+                    <p>E</p>
+                    <p>M</p>
+                    <p>E</p>
+                    <p>M</p>
+                    <p>B</p>
+                    <p>E</p>
+                    <p>R</p>
+                </section>
+            </main>
+        </div>
+
+
+    )
+}
+
+export default Title
