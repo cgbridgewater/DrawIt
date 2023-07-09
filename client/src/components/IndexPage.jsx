fa76384ddc277abef1e737215e0168a0098f6367
Chris Bridgewater <cgbridgewater@outlook.com>
2023-07-08 21:49:01 -0700

    adding content

--- client/src/components/IndexPage.jsx
+++ client/src/components/IndexPage.jsx
@@ -1,4 +1,7 @@
-
+import Canvas from "./CanvasPage"
+import Logo from "./LogoPage";
+import '../Style/Canvas.css';
+import '../Style/Logo.css';
 
 
 const Indexpage = () => {
@@ -6,10 +9,13 @@ const Indexpage = () => {
     return(    
 
         <div>
-            <h1>TEST!</h1>
+            <h1 className="banner">Can You <em>Remember?</em></h1>
+            <h1 className="banner-sm">Draw the logo from memory!!</h1>
+            <div className="flex" >
+                <Canvas/>
+                <Logo/>
+            </div>
         </div>
-
-
 
 
 
